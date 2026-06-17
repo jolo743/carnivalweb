40c5ba9ae0d9e0ad2372d84fcdc563b55074e5ec
jolo743 <jordigaar743@gmail.com>
2026-06-17 09:14:25 +0200

    navbar done

---
+++ games/parejas/script.js
@@ -0,0 +1,6 @@
+const cardIcons = ['rocket', 'controller', 'celebration', 'circus', 'popcorn', 'sharp-five', 'sparkle', 'ticket']
+
+let  cards= [...cardIcons, ...cardIcons]
+let flippedcards = []
+let matchedpairs = 0
+let score = 0
